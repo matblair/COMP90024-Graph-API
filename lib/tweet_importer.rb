64e2faa201917e0449d4777ca853513666b635e7
Mathew Blair <mathew.blair@unimelb.edu.au>
2015-05-15 22:26:14 +1000

    trying to work out why it's broken

--- lib/tweet_importer.rb
+++ lib/tweet_importer.rb
@@ -19,30 +19,35 @@ class TweetImporter
     to_couch = []
     errors = []
     users = []
+
     tweets.each do |tweet_hash|
       begin
         ## Create the tweet
         tweet_id  = tweet_hash[TWEET_ID_KEY]
         retweeted = tweet_hash[RETWEET_KEY]
-
+        puts "creating tweets"
         tweet = Tweet.new(twitter_id: tweet_id,
                           retweet: retweeted,
                           in_couch: true)
-
+        puts "built tweet"
         # Check if we have a tweet
         if tweet.save
+          puts "tweet saved"
           entities = tweet_hash["entities"]
+          puts "build entitites"
           # Is unique so build relationships
           user = find_tweeters tweet, tweet_hash
           find_topics tweet, entities
           find_mentions tweet, entities
           find_replies tweet, user, tweet_hash
 
+          puts "made some stuff"
           # Extract the user info for couch
           if tweet_hash.has_key? USER_KEY
             users << tweet_hash[USER_KEY]
           end
 
+          puts "added users"
           # Parse the date
           d = DateTime.parse(tweet_hash[CREATED_AT_KEY])
           # Build json for that
@@ -51,7 +56,7 @@ class TweetImporter
                        string: tweet_hash[CREATED_AT_KEY]}
           # Assign it
           tweet_hash[CREATED_AT_KEY] = date_hash
-
+          puts "built created at"
 
           # Push to couch for tweet content storage
           to_couch << tweet_hash
@@ -59,7 +64,8 @@ class TweetImporter
           errors << tweet.errors
         end
       rescue Exception => e
-        $stderr.puts e
+        puts "wtf happened here"
+        puts e
         # Just incase the above throws an exception (which it shouldn't...)
         errors << tweet_hash
       end
