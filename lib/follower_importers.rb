6a44e0088659992013f7778153095951ad56d149
Mathew Blair <mathew.blair@unimelb.edu.au>
2015-05-19 13:24:03 +1000

    added redundancy so it doesn't import twice

--- lib/follower_importers.rb
+++ lib/follower_importers.rb
@@ -1,20 +1,31 @@
 # Imports user followers by reading in a csv of user follower
 # details and then saving those relations in neo4j
+require 'csv'
 
 def add_follower user_id, follower_id
   u = User.find_by(twitter_id: user_id.to_s)
   f = User.find_by(twitter_id: follower_id.to_s)
+
   if f && u
-    u.followers << f
-    u.save
+  	if !(u.followers.include? f)
+	    u.followers << f
+	    u.save
+	    return true
+	else
+		return false
+	end
   end
 end
 
 def process_user_line user_id, followers
+	count = 0
 	fol_array = followers.strip.split(" ")
 	fol_array.each do |f_id|
-		add_follower user_id, f_id
+		if add_follower user_id, f_id
+			count += 1
+		end
 	end
+	count
 end
 
 # Read in the file
@@ -22,5 +33,6 @@ user_follwers = ARGV[0]
 
 # Open the file as csv with headers
 CSV.foreach(user_follwers, headers:true).each do |line|
-	process_user_line line['user_id'], line['followers']
+	count = process_user_line line['user_id'], line['followers']
+	puts "Successfully imported #{count} out of #{line['followers'].split(" ").count} followers for line['user_id']"
 end
